504640a419cf0806b77813d144934520f78aee89
YDJX <f258fengye@163.com>
2013-10-31 22:59:27 +0800

    add fm_file,this is freemind record.

---
+++ fm_file/JeeCMSProj.mm
@@ -0,0 +1,12 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1382424904399" ID="ID_109968396" MODIFIED="1382424932700" TEXT="JeeCMSProj">
+<node CREATED="1382424957430" ID="ID_1328542963" MODIFIED="1382424973990" POSITION="right" TEXT="StandardGenerateAppServiceImpl  152:21">
+<node CREATED="1382425044298" ID="ID_1702939044" MODIFIED="1382425082900" TEXT="if has existed &apos;&apos;&apos;  "/>
+</node>
+<node CREATED="1382425024928" ID="ID_1956974947" MODIFIED="1382425664381" POSITION="right" TEXT="BlobByteArrayTrans.blob2ByteArray( imageBean.getImageData() ) );"/>
+<node CREATED="1382426165862" ID="ID_1392758517" MODIFIED="1382427060116" POSITION="right" TEXT="YuLongCXGenerateAppServiceImpl 99:21 ">
+<node CREATED="1382426314754" ID="ID_1230068754" MODIFIED="1382426326786" TEXT="add next line break;"/>
+</node>
+</node>
+</map>
