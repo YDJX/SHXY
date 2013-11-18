9d359c0fc353c4538fafbdb45f7c2f63038ec6da
YDJX <f258fengye@163.com>
2013-11-18 16:30:10 +0800

    simple update

--- fm_file/JeeCMSProj.mm
+++ fm_file/JeeCMSProj.mm
@@ -1,12 +1,43 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1382424904399" ID="ID_109968396" MODIFIED="1382424932700" TEXT="JeeCMSProj">
-<node CREATED="1382424957430" ID="ID_1328542963" MODIFIED="1382424973990" POSITION="right" TEXT="StandardGenerateAppServiceImpl  152:21">
+<node CREATED="1382424904399" ID="ID_109968396" MODIFIED="1383554743644" TEXT="JeeCMSProj">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <div style="margin-left: 20px; padding-top: 2px">
+      <a class="header" href="eclipse-javadoc:%E2%98%82=JeeCMSProj/E:%5C/workspaces%5C/workspace%5C/JeeCMSProj%5C/WebContent%5C/WEB-INF%5C/lib%5C/myutil.jar%3Cutil">util</a>.<a class="header" href="eclipse-javadoc:%E2%98%82=JeeCMSProj/E:%5C/workspaces%5C/workspace%5C/JeeCMSProj%5C/WebContent%5C/WEB-INF%5C/lib%5C/myutil.jar%3Cutil(FTPUtil.class%E2%98%83FTPUtil">FTPUtil</a>.FTPUtil(<a class="header" href="eclipse-javadoc:%E2%98%82=JeeCMSProj/E:%5C/workspaces%5C/workspace%5C/JeeCMSProj%5C/WebContent%5C/WEB-INF%5C/lib%5C/myutil.jar%3Cutil(FTPUtil.class%E2%98%83FTPUtil~FTPUtil~Ljava.lang.String;~I~Ljava.lang.String;~Ljava.lang.String;%E2%98%82String">String</a>&#160;host, int port, <a class="header" href="eclipse-javadoc:%E2%98%82=JeeCMSProj/E:%5C/workspaces%5C/workspace%5C/JeeCMSProj%5C/WebContent%5C/WEB-INF%5C/lib%5C/myutil.jar%3Cutil(FTPUtil.class%E2%98%83FTPUtil~FTPUtil~Ljava.lang.String;~I~Ljava.lang.String;~Ljava.lang.String;%E2%98%82String">String</a>&#160;username, <a class="header" href="eclipse-javadoc:%E2%98%82=JeeCMSProj/E:%5C/workspaces%5C/workspace%5C/JeeCMSProj%5C/WebContent%5C/WEB-INF%5C/lib%5C/myutil.jar%3Cutil(FTPUtil.class%E2%98%83FTPUtil~FTPUtil~Ljava.lang.String;~I~Ljava.lang.String;~Ljava.lang.String;%E2%98%82String">String</a>&#160;password)
+    </div>
+    <p>
+      ftp.login();
+    </p>
+    <p>
+      if(GenerateDefines.PUBLISH_APP_CLEANSERVERFLAG)
+    </p>
+    <p>
+      ftp.cleanRemoteDir(remoteDir);
+    </p>
+    <p>
+      ftp.uploadFolder(appBean.getFolderPath(), remoteDir);
+    </p>
+    <p>
+      ftp.logout();
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1382424957430" ID="ID_1328542963" MODIFIED="1383554562298" POSITION="right" TEXT="StandardGenerateAppServiceImpl  152:21">
 <node CREATED="1382425044298" ID="ID_1702939044" MODIFIED="1382425082900" TEXT="if has existed &apos;&apos;&apos;  "/>
 </node>
 <node CREATED="1382425024928" ID="ID_1956974947" MODIFIED="1382425664381" POSITION="right" TEXT="BlobByteArrayTrans.blob2ByteArray( imageBean.getImageData() ) );"/>
-<node CREATED="1382426165862" ID="ID_1392758517" MODIFIED="1382427060116" POSITION="right" TEXT="YuLongCXGenerateAppServiceImpl 99:21 ">
+<node CREATED="1382426165862" ID="ID_1392758517" MODIFIED="1383554562299" POSITION="right" TEXT="YuLongCXGenerateAppServiceImpl 99:21 ">
 <node CREATED="1382426314754" ID="ID_1230068754" MODIFIED="1382426326786" TEXT="add next line break;"/>
 </node>
+<node CREATED="1383552751316" ID="ID_140976679" MODIFIED="1383552767889" POSITION="left" TEXT="FileUtils.readFileToString(file, &quot;utf-8&quot;);"/>
+<node CREATED="1383552832602" ID="ID_831235423" MODIFIED="1383552834476" POSITION="left" TEXT="CryptoUtil.DESEncrypt(String.valueOf(destInt), seed, &quot;utf-8&quot;);"/>
+<node CREATED="1383552842750" ID="ID_37376497" MODIFIED="1383552844164" POSITION="left" TEXT="FileUtils.writeStringToFile(file, dest, &quot;utf-8&quot;);"/>
+<node CREATED="1383554505939" ID="ID_498739793" MODIFIED="1383554537998" POSITION="left" TEXT="FileUtils.cleanDirectory(dstFolder);"/>
+<node CREATED="1383554513146" ID="ID_159691148" MODIFIED="1383554525871" POSITION="left" TEXT="FileUtils.copyDirectory(srcFolder, dstFolder);"/>
 </node>
 </map>
