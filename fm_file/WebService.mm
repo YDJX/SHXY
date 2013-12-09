d7a2b4244695799e81d2e31d3b8da24178eab817
YDJX <f258fengye@163.com>
2013-12-09 23:21:57 +0800

    update

---
+++ fm_file/WebService.mm
@@ -0,0 +1,47 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1385564152772" ID="ID_1157718877" MODIFIED="1385564841782" TEXT="WebService">
+<node CREATED="1385564844451" ID="ID_297225542" MODIFIED="1385564868864" POSITION="right" TEXT="AXIS"/>
+<node CREATED="1385564851296" ID="ID_82943095" MODIFIED="1385564861574" POSITION="right" TEXT="CXF">
+<node CREATED="1385566190553" ID="ID_603018248" MODIFIED="1385566198698" TEXT="bean&#x6a21;&#x578b;"/>
+<node CREATED="1385564879041" ID="ID_282185464" MODIFIED="1385564904939" TEXT="Interface&#x8981;&#x53d1;&#x5e03;&#x7684;&#x63a5;&#x53e3;"/>
+<node CREATED="1385566201362" ID="ID_505491492" MODIFIED="1385566231795" TEXT="InterfaceImplement&#x63a5;&#x53e3;&#x5b9e;&#x73b0;"/>
+<node CREATED="1385564907464" ID="ID_1649024189" MODIFIED="1385564919250" TEXT="Server&#x670d;&#x52a1;&#x5668;&#x7aef;">
+<node CREATED="1385564934001" ID="ID_1203127256" MODIFIED="1385565058539" TEXT="&#x65b9;&#x6cd5;&#x540d;&#x79f0;&#x7684;bean"/>
+<node CREATED="1385565059721" ID="ID_827011162" MODIFIED="1385565082663" TEXT="&#x65b9;&#x6cd5;&#x540d;&#x79f0;&#x8fd4;&#x56de;&#x503c;&#x7684;bean"/>
+<node CREATED="1385565206912" ID="ID_1125653431" MODIFIED="1385565213867" TEXT="ObjectFactory">
+<node CREATED="1385565257752" ID="ID_1147892912" MODIFIED="1385565331457" TEXT="&#x6839;&#x636e;QName &#x521b;&#x5efa;&#x5b9e;&#x4f8b;&#x3001;&#x521b;&#x5efa;&#x7ed1;&#x5b9a;&#x5b9e;&#x4f8b;"/>
+</node>
+<node CREATED="1385565372224" ID="ID_413581326" MODIFIED="1385565415426" TEXT="&#x670d;&#x52a1;&#x5668;&#x7aef;&#x7684;bean"/>
+<node CREATED="1385565420953" ID="ID_980963302" MODIFIED="1385565430090" TEXT="&#x670d;&#x52a1;&#x5668;&#x7aef;&#x7684;&#x5305;&#x4fe1;&#x606f;"/>
+<node CREATED="1385565487917" ID="ID_1308291193" MODIFIED="1385565505112" TEXT="&#x670d;&#x52a1;&#x5668;&#x7aef;&#x63a5;&#x53e3;  &#x7ee7;&#x627f;Service">
+<node CREATED="1385565649684" ID="ID_1113062526" MODIFIED="1385565709145" TEXT="&#x901a;&#x8fc7;QName&#x548c;wsdl&#x6587;&#x4ef6;&#x6784;&#x9020;Service&#x5b9e;&#x4f8b;"/>
+</node>
+<node CREATED="1385565507097" ID="ID_1668630342" MODIFIED="1385565817298" TEXT="&#x670d;&#x52a1;&#x5668;&#x7aef;&#x63a5;&#x53e3;&#x7aef;&#x53e3;&#x7c7b;&#x578b;">
+<node CREATED="1385565818688" ID="ID_860307826" MODIFIED="1385565965571" TEXT="&#x91cc;&#x9762;&#x662f;&#x8981;&#x53d1;&#x5e03;&#x7684;&#x63a5;&#x53e3;&#x76f8;&#x540c;&#x7684;&#x65b9;&#x6cd5;&#xff08;&#x670d;&#x52a1;&#x5668;&#x7aef;&#x63a5;&#x53e3;&#xff09;"/>
+</node>
+<node CREATED="1385565891481" ID="ID_1138047676" MODIFIED="1385565907050" TEXT="&#x670d;&#x52a1;&#x5668;&#x7aef;&#x63a5;&#x53e3;&#x7aef;&#x53e3;&#x7c7b;&#x578b;&#x7684;&#x5b9e;&#x73b0;">
+<node CREATED="1385566000977" ID="ID_358730611" MODIFIED="1385566087010" TEXT="&#x8fd9;&#x91cc;&#x662f;&#x5b9e;&#x73b0;&#x670d;&#x52a1;&#x5668;&#x7aef;&#x53e3;&#x63a5;&#x53e3;&#x7c7b;&#x578b;&#x7684;&#x5b9e;&#x73b0;&#x7c7b;&#xff0c;&#x9700;&#x8981;&#x4fee;&#x6539;&#x771f;&#x6b63;&#x8981;&#x5904;&#x7406;&#x7684;&#x903b;&#x8f91;&#x6216;&#x8005;&#x8c03;&#x7528;&#x8981;&#x53d1;&#x5e03;&#x7684;&#x63a5;&#x53e3;&#xff08;&#x524d;&#x63d0;&#x662f;&#x6b64;&#x63a5;&#x53e3;&#x6709;&#x786e;&#x5b9e;&#x7684;&#x5b9e;&#x73b0;&#xff09;"/>
+</node>
+</node>
+<node CREATED="1385564920433" ID="ID_1203051082" MODIFIED="1385564930597" TEXT="Client&#x5ba2;&#x6237;&#x7aef;">
+<node CREATED="1385566111825" ID="ID_1887683256" MODIFIED="1385566134791" TEXT="&#x65b9;&#x6cd5;&#x540d;&#x79f0;&#x7684;bean"/>
+<node CREATED="1385566135289" ID="ID_1694438602" MODIFIED="1385566144882" TEXT="&#x65b9;&#x6cd5;&#x540d;&#x79f0;&#x8fd4;&#x56de;&#x503c;&#x7684;bean"/>
+<node CREATED="1385566145385" ID="ID_708387369" MODIFIED="1385566155746" TEXT="ObjectFactory">
+<node CREATED="1385566156665" ID="ID_108491760" MODIFIED="1385566169992" TEXT="&#x6839;&#x636e;QName &#x521b;&#x5efa;&#x5b9e;&#x4f8b;&#x3001;&#x521b;&#x5efa;&#x7ed1;&#x5b9a;&#x5b9e;&#x4f8b;"/>
+</node>
+<node CREATED="1385566172001" ID="ID_368187141" MODIFIED="1385566179787" TEXT="&#x5ba2;&#x6237;&#x7aef;&#x7684;bean"/>
+<node CREATED="1385566241617" ID="ID_1023166913" MODIFIED="1385566263114" TEXT="&#x5ba2;&#x6237;&#x7aef;&#x7684;&#x5305;&#x4fe1;&#x606f;"/>
+<node CREATED="1385565487917" ID="ID_1938940155" MODIFIED="1385566360498" TEXT="&#x5ba2;&#x6237;&#x7aef;&#x63a5;&#x53e3;  &#x7ee7;&#x627f;Service">
+<node CREATED="1385565649684" ID="ID_609900755" MODIFIED="1385566444423" TEXT="&#x901a;&#x8fc7;QName&#x548c;wsdl&#x6587;&#x4ef6;&#x6784;&#x9020;Service&#x5b9e;&#x4f8b;&#xff0c;&#x5145;&#x5f53;&#x63a5;&#x53e3;"/>
+</node>
+<node CREATED="1385565507097" ID="ID_47305340" MODIFIED="1385566368482" TEXT="&#x5ba2;&#x6237;&#x5668;&#x7aef;&#x63a5;&#x53e3;&#x7aef;&#x53e3;&#x7c7b;&#x578b;">
+<node CREATED="1385565818688" ID="ID_1738330012" MODIFIED="1385566384004" TEXT="&#x91cc;&#x9762;&#x662f;&#x8981;&#x53d1;&#x5e03;&#x7684;&#x63a5;&#x53e3;&#x76f8;&#x540c;&#x7684;&#x65b9;&#x6cd5;&#xff08;&#x5ba2;&#x6237;&#x5668;&#x7aef;&#x63a5;&#x53e3;&#xff09;"/>
+</node>
+<node CREATED="1385565891481" ID="ID_1227139386" MODIFIED="1385566590431" TEXT="&#x5ba2;&#x6237;&#x5668;&#x7aef;&#x63a5;&#x53e3;&#x8c03;&#x7528;&#x5b9e;&#x73b0;">
+<node CREATED="1385566000977" ID="ID_1569925329" MODIFIED="1385566784713" TEXT="&#x8fd9;&#x91cc;&#x662f;&#x8c03;&#x7528;&#x63a5;&#x53e3;&#xff0c;&#x751f;&#x6210;&#x4ee3;&#x7801;&#x5c55;&#x793a;&#x4e86;&#x5982;&#x4f55;&#x4f7f;&#x7528;&#xff0c;&#x5229;&#x7528;&apos;&#x5ba2;&#x6237;&#x7aef;&#x63a5;&#x53e3;&apos;&#x4e2d;&#x6784;&#x9020;&#x51fd;&#x6570;&#x83b7;&#x5f97;&#x63a5;&#x53e3;&#x5b9e;&#x4f8b;&#xff0c;&#x5e76;&#x901a;&#x8fc7;get*Port&#x6765;&#x83b7;&#x53d6;&#x7aef;&#x53e3;&#xff0c;&#x518d;&#x8c03;&#x7528;&#x76f8;&#x5e94;&#x65b9;&#x6cd5;&#xff0c;&#x5c31;&#x5b9e;&#x73b0;&#x4e86;&#x8c03;&#x7528;&#x670d;&#x52a1;&#x5668;&#x7aef;&#x63a5;&#x53e3;&#x5b9e;&#x73b0;&#x7684;&#x6548;&#x679c;&#x3002;"/>
+</node>
+</node>
+</node>
+</node>
+</map>
